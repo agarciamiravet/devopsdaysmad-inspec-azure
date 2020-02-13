302bd88a2c76a45e02dcb54d22d7c6ea7024ac6a
Alejandro Garcia Miravet <agarciamiravet@gmail.com>
2020-02-13 08:43:18 +0100

    Update certificates.rb

--- src/inspec/devopsdaysmad-inspec-app/controls/certificates.rb
+++ src/inspec/devopsdaysmad-inspec-app/controls/certificates.rb
@@ -9,6 +9,6 @@ control "certificate-check-validity" do
         its('issuer.CN') { should eq ca }
         its('signature_algorithm') { should cmp 'sha256WithRSAEncryption' }
         its('subject.CN') { should eq site }
-        its('validity_in_days') { should be > 90}
+        its('validity_in_days') { should be > 30}
     end
 end

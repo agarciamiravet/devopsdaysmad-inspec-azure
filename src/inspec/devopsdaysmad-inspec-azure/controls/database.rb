525a6973ba21f8b660f889e93fe329eec80217f6
alex <alexalex.com>
2020-02-16 09:59:47 +0100

    ~Modify tests titles

--- src/inspec/devopsdaysmad-inspec-azure/controls/database.rb
+++ src/inspec/devopsdaysmad-inspec-azure/controls/database.rb
@@ -5,8 +5,6 @@ sql_server_name = 'pasionporlosbitsdbserver'
 sql_database_name = 'pasionporlosbitsdatabase'
 
 control 'azure_sql_database_pasionporlosbits' do
-  title 'Check security settings of database'
-    desc "Test settings of pasionporlosbits database"
     describe azurerm_sql_database(resource_group: resource_group, server_name: sql_server_name, database_name: sql_database_name) do
         it            { should exist }
         its('name')   { should eq sql_database_name}
--- src/inspec/devopsdaysmad-inspec-azure/controls/webapp.rb
+++ src/inspec/devopsdaysmad-inspec-azure/controls/webapp.rb
@@ -3,7 +3,6 @@ title "Check web app pasionporlosbits"
 resource_group = 'rg-devopsdays-pasion'
 
 control "azure_webapp_pasionporlosbits"
-title 'Check security settings of webapp'
 describe azurerm_webapp(resource_group: resource_group, name: 'pasiondebits') do
   it { should exist }
   its('properties.httpsOnly') { should cmp true }

f645666f46919fe1a442af9960360edfe403c63d
alex <alexalex.com>
2020-02-19 19:18:29 +0100

    ~test changes

--- src/inspec/devopsdaysmad-inspec-app/controls/httpheaders.rb
+++ src/inspec/devopsdaysmad-inspec-app/controls/httpheaders.rb
@@ -10,9 +10,9 @@ end
 
 control 'web_check_httpstatus' do
      describe http(site) do
-         its ('headers.X-XSS-Protection') { should cmp '1; mode=block'}
+         its ('headers.X-XSS-Protection') { should cmp '1; mode=TOP'}
          its ('headers.Set-Cookie') { should match /HttpOnly/}
-         its ('headers.X-Frame-Options') { should cmp 'DENY' }
+         its ('headers.X-Frame-Options') { should cmp 'ALLOW' }
 
      end
  end

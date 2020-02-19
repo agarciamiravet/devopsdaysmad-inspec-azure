e71e86af9db9a3e764640a9238279cd7c08c7a0a
alex <alexalex.com>
2020-02-19 21:50:21 +0100

    Add new database tests

--- src/inspec/devopsdaysmad-inspec-azure/controls/database.rb
+++ src/inspec/devopsdaysmad-inspec-azure/controls/database.rb
@@ -8,6 +8,15 @@ resource_group = terraformsParams['resource_group_name']['value']
 sql_server_name = terraformsParams['database_server_name']['value']
 sql_database_name = terraformsParams['sql_database_name']['value']
 
+control 'azure_sql_server_pasionporlosbits' do
+  describe azurerm_sql_server(resource_group: resource_group, server_name: sql_server_name) do
+      it            { should exist }
+      its('name')   { should eq sql_server_name}
+      its('auditing_settings.properties') { should have_attributes(state: "Enabled")}
+      its('auditing_settings.properties') { should have_attributes(retentionDays: 7)}
+    end
+end
+
 control 'azure_sql_database_pasionporlosbits' do
     describe azurerm_sql_database(resource_group: resource_group, server_name: sql_server_name, database_name: sql_database_name) do
         it            { should exist }
@@ -15,7 +24,7 @@ control 'azure_sql_database_pasionporlosbits' do
         its('location') { should eq "westeurope" }
         its('sku.name') { should eq "Standard" }
         its('properties.collation') { should eq "SQL_Latin1_General_CP1_CI_AS"}
-        its('auditing_settings') { should have_attributes(retentionDays: 0)}
-        its('encryption_protector') { should cmp 'aa'  }
+        its('auditing_settings.properties') { should have_attributes(state: "Enabled")}
+        its('auditing_settings.properties') { should have_attributes(retentionDays: 7)}
       end
 end
